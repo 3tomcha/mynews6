3ad34bc75229cf75703a61aafbe1cf6ee5d5bac3
EC2 Default User <ec2-user@ip-172-31-6-83.us-east-2.compute.internal>
2019-09-16 07:16:53 +0000

    まずは継承できるかどうかを試す

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <div id="app">
+            {{--画面上部に表示するナビゲーションバーです--}}
+        </div>
+        <main>
+            @yield('content')
+        </main>
+    </body>
+</html>
