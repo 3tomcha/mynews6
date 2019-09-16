e6909f5937581d125cd157cef0eeba861fb4097b
EC2 Default User <ec2-user@ip-172-31-6-83.us-east-2.compute.internal>
2019-09-16 07:25:46 +0000

    yieldに当てはめることができた

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,1 +1,14 @@
 @extends('layouts.admin')
+
+@section('title', 'ニュースの新規作成') 
+
+
+@section('content')
+<div>
+    <div>
+        <div>
+            <h2>ニュース新規作成</h2>
+        </div>
+    </div>
+</div>
+@endsection
